528f0648155ef79ce8c4288846c83dadfcd8d3e5
adeeadelina <moisiadelina@gmail.com>
2020-07-15 14:13:55 +0300

    Added equals method.

--- src/main/Bills.java
+++ src/main/Bills.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Bills {
     public int nrOfBills;
     public int typeOfBills;
@@ -5,6 +7,20 @@ public class Bills {
     public Bills(int nrOfBills, int typeOfBills) {
         this.nrOfBills = nrOfBills;
         this.typeOfBills = typeOfBills;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bills bills = (Bills) o;
+        return nrOfBills == bills.nrOfBills &&
+                typeOfBills == bills.typeOfBills;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nrOfBills, typeOfBills);
     }
 
     public String toString() {
